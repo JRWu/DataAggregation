79747b82d6a905b4973fa3a7510d135de9bacf69
Jia Wu <jia_wu@hotmail.ca>
2015-10-25 11:25:07 -0400

    Adding changes for merge.

--- Project/AppleProject/load_csv.cpp
+++ Project/AppleProject/load_csv.cpp
@@ -28,3 +28,24 @@ void LoadCSV::on_analyze_btn_clicked()
     analyze_csv_page = new AnalyzeCSV();
     this->setCentralWidget(analyze_csv_page);
 }
+
+void LoadCSV::on_publication_btn_clicked()
+{
+    // This is what happens when user clicks the button for Publications
+    std::cout << "Publication button registered." << "\n";
+    QString filename=QFileDialog::getOpenFileName(
+                this,
+                tr("Open File"),        // Dialog for prompt
+                "C://",                 // Default folder to open
+                "CSV Files (*.csv)"     // File extension to filter for
+                );
+
+
+    //vvv Comment out this if don't want popup vvv
+    QMessageBox::information(
+                this,
+                tr("File Name: "),
+                filename
+                );
+    //^^^ Comment out this if don't want popup ^^^
+}
